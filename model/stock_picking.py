faecd64819f11a3c71a3ebac4b2afce6de75d2f9
yousentech <83219678+yousentech@users.noreply.github.com>
2025-09-21 16:41:44 +0300

    update

--- model/stock_picking.py
+++ model/stock_picking.py
@@ -7,7 +7,7 @@ _logger = logging.getLogger(__name__)
 class stock_picking(models.Model):
     _inherit = 'stock.picking'
 
-    prevent_update_source_des_location = fields.Boolean(compute='_check_group_prevent_update_source_des_location',)
+    prevent_update_source_des_location = fields.Boolean()
 
     def _get_location_dest_domain(self):
         domain = []
